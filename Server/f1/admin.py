d26bca192ef2d05e178e4bb89165f5954dca4ae6
AlexanderBastowos <alexander.bastowros56@gmail.com>
2023-12-26 20:08:39 -0500

    str method for models

--- Server/f1/admin.py
+++ Server/f1/admin.py
@@ -3,10 +3,15 @@ from .models import *
 
 @admin.register(RaceResultDriver)
 class RaceDriverPositionsAdmin(admin.ModelAdmin):
-    list_display=("driver_name", "position", "points")
+    list_display=("race" ,"driver_name", "position", "points")
+    # fields=["driver"]
+
+    # def race_name(self, obj):
+    #     return obj.race.name
 
     def driver_name(self, obj):
-        return obj.get_driver_name()
+        return obj.driver.name
+    
 
 # Register your models here.
 admin.site.register(User)
--- Server/f1/models.py
+++ Server/f1/models.py
@@ -1,50 +1,73 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+class BaseModel(models.Model):
+    created_at = models.DateTimeField("Created On", auto_now_add=True)
+    updated_on = models.DateTimeField("Updated On", auto_now=True)
+    
+    class Meta:
+        abstract = True
+
+
 '''
 F1APP User Models
 '''
 class User(AbstractUser):
     test = models.BooleanField(default=False)
+    
 
 # league configs ties to users
-class League(models.Model):
+class League(BaseModel):
     pass
 
 
 '''
 F1 Specific Data
 '''
-class Team(models.Model):
+class Team(BaseModel):
     name = models.CharField(max_length=200, blank=False, null=False)
     points = models.IntegerField(default=0)
     # points 
 
-class Driver(models.Model):
+    def __str__(self):
+        return self.name
+
+class Driver(BaseModel):
     name = models.CharField(max_length=200, blank=False, null=False)
     points = models.IntegerField(default=0, null=False)
     team = models.ForeignKey(Team, on_delete=models.CASCADE)
 
-class Season(models.Model):
+    def __str__(self):
+        return self.name
+
+class Season(BaseModel):
     year = models.IntegerField(default=0, null=False)
 
-class QualifiyingResults(models.Model):
+    def __str__(self):
+        return str(self.year)
+
+class QualifiyingResults(BaseModel):
     pass
 
-class Circuit(models.Model):
+class Circuit(BaseModel):
     name = models.CharField(max_length=200, blank=False, null=False)
     # TODO: number of laps
     # track length
     # Sector 1, 2, 3
     # DRS zones
+    def __str__(self):
+        return self.name
 
-class RaceResult(models.Model):
+class RaceResult(BaseModel):
     circuit = models.OneToOneField(Circuit, on_delete=models.CASCADE)
-    season = models.OneToOneField(Season, on_delete=models.CASCADE)
+    season = models.ForeignKey(Season, on_delete=models.CASCADE)
 
-class RaceResultDriver(models.Model):
-    driver = models.OneToOneField(Driver, on_delete=models.CASCADE)
-    position = models.IntegerField(default=0, null=False, unique=True)
+    def __str__(self):
+        return f"{self.season.year} {self.circuit.name}"
+
+class RaceResultDriver(BaseModel):
+    driver = models.ForeignKey(Driver, on_delete=models.CASCADE)
+    position = models.IntegerField(default=0, null=False, unique=False)
     race = models.ForeignKey(RaceResult, on_delete=models.CASCADE)
     did_not_finish = models.BooleanField(default=False)
     # only get fastest lap points if in top 10
@@ -53,6 +76,9 @@ class RaceResultDriver(models.Model):
     laps = models.IntegerField(default=0, null=False)
     # Race Time in miliseconds
     time = models.IntegerField(default=0, null=False)
+
+    def __str__(self):
+        return self.driver.name
 
     @property
     def points(self):
@@ -75,12 +101,9 @@ class RaceResultDriver(models.Model):
         if(position_to_points[self.position]):
             points += position_to_points[self.position]
         return points
-    
-    def get_driver_name(self):
-        return self.driver.name
 
 # this can probaly just be derived from driver results, based on team drivers and their positions 
-class RaceResultTeam(models.Model):
+class RaceResultTeam(BaseModel):
     team = models.ForeignKey(Team, on_delete=models.CASCADE)
     
     @property
