d2688027ae36d90ff3bca85999511b09ffbe4518
AlexanderBastowos <alexander.bastowros56@gmail.com>
2023-12-28 20:06:57 -0500

    fantasy models on admin

--- Server/f1/urls.py
+++ Server/f1/urls.py
@@ -4,5 +4,5 @@ from .views import user_view
 urlpatterns = [
     path('users/', user_view.UserListView.as_view()),
     path('user/', user_view.UserView.as_view()),
-    path('leagues/', user_view.UserLeagues.as_view())
+    path('user/leagues/', user_view.UserLeagues.as_view())
 ]
