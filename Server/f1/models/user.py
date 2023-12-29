cd02a411f8cd3301ccce2c741510ae53989ff84a
dbeigi <dbeigi1995@gmail.com>
2023-12-29 00:37:49 -0500

    user model update

--- Server/f1/models/user.py
+++ Server/f1/models/user.py
@@ -2,5 +2,5 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
-    pass
-    
+    #keeping this for testing purposes, will remove after
+    REQUIRED_FIELDS = []
