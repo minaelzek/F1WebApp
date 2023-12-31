59156731dce4ab1a775fed5f900a799d9fbf87dd
dbeigi <dbeigi1995@gmail.com>
2023-12-30 23:51:32 -0500

    moved login functionality to its own file

--- Server/f1/urls.py
+++ Server/f1/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import user_view
+from .views import user_view, login_view
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
@@ -9,9 +9,9 @@ urlpatterns = [
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     # APIS
     path('user/<int:user_id>', user_view.UserView.as_view()),
-    path('register/user/', user_view.RegisterUserView.as_view()),
-    path('login/user/', user_view.LoginUserSerializer.as_view()),
-    path('logout/user/', user_view.LoginUserSerializer.as_view()),
+    path('register/user/', login_view.RegisterUserView.as_view()),
+    path('login/user/', login_view.LoginUserSerializer.as_view()),
+    path('logout/user/', login_view.LogoutUserView.as_view()),
     path('user/<int:user_id>/leagues/', user_view.UserLeagues.as_view()),
     path('user/<int:user_id>/league/', user_view.UserCreateLeague.as_view()),
     path('user/<int:user_id>/league/<int:league_id>', user_view.UserLeague.as_view())
---
+++ Server/f1/views/login_view.py
@@ -0,0 +1,48 @@
+from rest_framework.views import APIView
+from rest_framework import status, permissions
+from rest_framework.response import Response
+from rest_framework.authentication import SessionAuthentication
+from django.contrib.auth import authenticate, login, logout
+from drf_spectacular.utils import extend_schema
+from ..serlializers.user_serlializer import UserSerializer
+from ..serlializers.user_serlializer import LoginUserSerializer
+from drf_spectacular.utils import extend_schema
+
+class RegisterUserView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    @extend_schema(request=UserSerializer, responses={201: UserSerializer, 400: None})
+    def post(self, request, *args, **kwargs):
+        serializer = UserSerializer(data=request.data, many=False)
+        if serializer.is_valid():
+            user = serializer.create(serializer.data)
+            if user:
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LoginUserView(APIView):
+    permission_classes = [permissions.AllowAny]
+    authentication_classes = [SessionAuthentication]
+
+    @extend_schema(request=LoginUserSerializer, responses=None)
+    def post(self, request, *args, **kwargs):
+        serializer = LoginUserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = authenticate(username=request.data.get('username'), password=request.data.get('password'))
+            if user:
+                login(request, user)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            else:
+                return Response(serializer.errors, status=status.HTTP_401_UNAUTHORIZED)
+        
+class LogoutUserView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [SessionAuthentication]
+
+    # TODO: need to add extend_schema to this
+    def post(self, request):
+        logout(request)
+        return Response(status=status.HTTP_200_OK)
+
+
